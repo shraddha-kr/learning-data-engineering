6add11ce7f607b756873862ce2ba4e94ea6468b5
shraddha-kr <shraddha.kharangate@gmail.com>
2023-04-13 12:56:59 +0530

    Apr 13

--- fundamentals/sql/headfirst.sql
+++ fundamentals/sql/headfirst.sql
@@ -1,3 +1,6 @@
+/*Check service status*/
+sudo systemctl status postgresql.service
+
 /*Start postgres cli*/
 sudo -u postgres psql
 
@@ -12,5 +15,20 @@ CREATE DATABASE "HeadFirst"
 /*List all databases on a postgres server*/   
 \l
 
-/**/
+/*Delete a Database*/
 DROP DATABASE 'HeadFirst';
+
+/*Create DB*/
+CREATE DATABASE gregs_list;
+
+/*Switch or Connect to a Database*/
+\connect gregs_list;  
+
+/*Create Table*/
+CREATE Table doughnut_list(
+    doughnut_name VARCHAR(10),
+    doughnut_type VARCHAR(6)
+);
+
+/*Describe Table*/  
+\d  doughnut_list;
